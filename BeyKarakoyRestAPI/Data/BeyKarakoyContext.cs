8592b1035c32fd3b3325433f02ad7bfdff3345f9
semaguroll <semaguroll@gmail.com>
2020-03-30 21:55:23 +0300

    first commit

---
+++ BeyKarakoyRestAPI/Data/BeyKarakoyContext.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeyKarakoyRestAPI.Data
+{
+    public class BeyKarakoyContext : DbContext
+    {
+
+        public BeyKarakoyContext(DbContextOptions<BeyKarakoyContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<BeyKarakoyRestAPI.Models.Category> Category { get; set; }    
+        //public DbSet<BeyKarakoyRestAPI.Models.ProductModel> Product { get; set; }
+
+    }
+}
---
+++ BeyKarakoyRestAPI/Models/CategoryContext.cs
@@ -0,0 +1,20 @@
+﻿
+using BeyKarakoyRestAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeyKarakoyRestAPI.Models
+{
+    public class CategoryContext : DbContext
+    {
+        public CategoryContext(DbContextOptions<CategoryContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
